afbba8b0ca6d0a2aa21a5b5f054c8e3917e803fa
Alejandro Celaya <alejandrocelaya@gmail.com>
2026-07-26 10:10:24 +0200

    Migrate from ESLint to Oxlint and Oxfmt

--- src/count-by/index.ts
+++ src/count-by/index.ts
@@ -1,11 +1,12 @@
 /**
  * Count elements of an array based on provided function
  */
-export const countBy = <T>(list: readonly T[], fn: (a: T) => string | number): Record<string, number> => list.reduce(
-  (count, current) => {
-    const key = fn(current);
-    count[key] = (count[key] || 0) + 1;
-    return count;
-  },
-  {} as Record<string, number>,
-);
+export const countBy = <T>(list: readonly T[], fn: (a: T) => string | number): Record<string, number> =>
+  list.reduce(
+    (count, current) => {
+      const key = fn(current);
+      count[key] = (count[key] || 0) + 1;
+      return count;
+    },
+    {} as Record<string, number>,
+  );
--- src/zip-obj/index.ts
+++ src/zip-obj/index.ts
@@ -2,6 +2,4 @@
  * Create an object from a list of keys and values
  */
 export const zipObj = <T, K extends string>(keys: readonly K[], values: readonly T[]): { [P in K]: T } =>
-  Object.fromEntries(
-    keys.map((key, index) => [key, values[index]]),
-  ) as { [P in K]: T };
+  Object.fromEntries(keys.map((key, index) => [key, values[index]])) as { [P in K]: T };
